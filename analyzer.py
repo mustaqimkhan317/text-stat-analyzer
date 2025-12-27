3747acb42bb4b1ff5c1c17c3567010ebd1812cda
Mustaqim Khan <mustaqimkhan317@gmail.com>
2025-12-27 15:10:31 -0500

    Added function to retrieve common words

--- analyzer.py
+++ analyzer.py
@@ -3,9 +3,10 @@ import re
 class Analyzer:
     ''' Analyze text files and extract words '''
     def __init__(self):
-        self.words = []
+        self.words = [] # List for each word in the txt
 
     def read_file(self):
+        "Read the txt file or raise exception"
         try:
             with open("data.txt", "r", encoding="utf-8") as file:
                 return file.read()
@@ -15,15 +16,29 @@ class Analyzer:
             raise
     
     def extract_words(self, text):
+        "Extract all the words, remove spaces and punctuations"
         self.words = re.findall(r"\b\w+\b", text.lower())
         return self.words
+    
+    def common_words(self):
+        "A function for the most common words in the data file"
+        common = {}
+
+        for value in self.words:
+            if value not in common:
+                common[value] = 1
+            else:
+                common[value] += 1    
+        
+        return common
 
 
 def main():
     analyze = Analyzer()
     text = analyze.read_file()
     words = analyze.extract_words(text)
-    print(words)
+    common = analyze.common_words()
+    print(common)
 
 if __name__ == "__main__":
     main()
